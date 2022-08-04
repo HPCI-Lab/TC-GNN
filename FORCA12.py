93500348c81acdef3800c9dff9e25b3d0531de3b
massimilianofronza <maxfronza97@gmail.com>
2022-08-04 20:50:48 +0200

    Selection of area in FORCA dataset

--- FORCA12.py
+++ FORCA12.py
@@ -1,4 +1,5 @@
 import xarray as xr
+import matplotlib.pyplot as plt
 
 PATHS = []
 with open('path_hdd.ini', 'r') as file:
@@ -19,3 +20,30 @@ print()
 d = data_ssh.data_vars
 for key in d:
     print(key, '::', d[key].long_name)
+
+### Plotting test(in this script the data I'm handling has values and mesh in separate files)
+model_lon = data_mesh.lon.values
+model_lat = data_mesh.lat.values
+
+# The data is the sea surface height(ssh) for the first day(0) of year 2010
+data_sample = data_ssh.ssh[0].values
+
+step = 10
+plt.figure(figsize=(20, 10))
+plt.scatter(model_lon[::step], model_lat[::step], s=1, c=data_sample[::step])
+plt.colorbar(orientation='horizontal', pad=0.04)
+plt.savefig('world_FORCA12.png')
+
+# Extraction of a region like in 'offline_test.py'. Mediterranean sea here
+left = -10
+right = 40
+bottom = 30
+top = 47
+region_mask = (model_lon > left) & (model_lon < right) & (model_lat < top) & (model_lat > bottom)
+
+step = 1
+plt.figure(figsize=(20, 10))
+plt.scatter(model_lon[region_mask][::step], model_lat[region_mask][::step], s=1, c=data_sample[region_mask][::step])
+cbar = plt.colorbar(orientation='horizontal', pad=0.05)
+cbar.set_label(data_ssh.data_vars['ssh'].description)
+plt.savefig('mediterranean_FORCA12.png')
