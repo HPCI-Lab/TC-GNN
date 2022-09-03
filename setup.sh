3bc0df687d5ef089e57f9b3694d3692af4431193
massimilianofronza <maxfronza97@gmail.com>
2022-09-03 09:03:58 +0200

    Add dependencies setup for PyTorch Geometric

--- setup.sh
+++ setup.sh
@@ -10,3 +10,18 @@ pip install netcdf4
 pip install matplotlib
 pip install dask
 
+## PyTorch setup:
+
+pip install torch
+# Ensure at least PyTorch 1.11.0 and CUDA are installed:
+# python -c "import torch; print(torch.__version__)"	# 1.12.1+cu102
+# python -c "import torch; print(torch.version.cuda)"	# 10.2
+
+# The following torch and cuda versions are the ones verified in the previous step
+pip install torch-scatter -f https://data.pyg.org/whl/torch-1.12.1+cu102.html
+pip install torch-sparse -f https://data.pyg.org/whl/torch-1.12.1+cu102.html
+pip install torch-geometric
+
+# Optional(remember to change the torch and cuda version) dependencies:
+# pip install torch-cluster -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
+# pip install torch-spline-conv -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
