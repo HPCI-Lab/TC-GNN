440f3a01f72d1df2f2696da1aaaa89ffc286a377
massimilianofronza <maxfronza97@gmail.com>
2022-10-01 19:07:09 +0200

    Variables renaming

--- src/pilot_data_creation.py
+++ src/pilot_data_creation.py
@@ -1,11 +1,11 @@
 import xarray as xr
 
-data_mesh = xr.open_dataset('./data/fesom.mesh.diag.nc')
-data_ssh = xr.open_dataset('./data/ssh.fesom.2010.nc')
+mesh_data = xr.open_dataset('../data/fesom.mesh.diag.nc')
+data_ssh = xr.open_dataset('../data/ssh.fesom.2010.nc')
 
 # Extracting longitude and latitude values of the mesh
-model_lon = data_mesh.lon.values
-model_lat = data_mesh.lat.values
+model_lon = mesh_data.lon.values
+model_lat = mesh_data.lat.values
 
 # The area we're gonna keep is the Mediterranean sea
 left = -10
@@ -15,34 +15,34 @@ top = 47
 region_mask = (model_lon > left) & (model_lon < right) & (model_lat < top) & (model_lat > bottom)
 
 # Changing the lon and lat values in the dataset
-data_mesh = data_mesh.drop_vars('lon')
-data_mesh = data_mesh.drop_vars('lat')
-data_mesh = data_mesh.assign_coords(lon=('spatial_subset', model_lon[region_mask])) # could also be 'nodes_subset'
-data_mesh = data_mesh.assign_coords(lat=('spatial_subset', model_lat[region_mask]))
+mesh_data = mesh_data.drop_vars('lon')
+mesh_data = mesh_data.drop_vars('lat')
+mesh_data = mesh_data.assign_coords(lon=('spatial_subset', model_lon[region_mask])) # could also be 'nodes_subset'
+mesh_data = mesh_data.assign_coords(lat=('spatial_subset', model_lat[region_mask]))
 
 # Taking just the surface depth value, dropping the useless values
-data_mesh = data_mesh.drop_vars('nz1')
-data_mesh = data_mesh.sel(nz=0.0, method='nearest')
+mesh_data = mesh_data.drop_vars('nz1')
+mesh_data = mesh_data.sel(nz=0.0, method='nearest')
 
-data_mesh = data_mesh.drop_vars('edge_cross_dxdy')
-data_mesh = data_mesh.drop_vars('edge_tri')
-data_mesh = data_mesh.drop_vars('edges')
-data_mesh = data_mesh.drop_vars('elem_area')
-data_mesh = data_mesh.drop_vars('elem_part')
-data_mesh = data_mesh.drop_vars('gradient_sca_x')
-data_mesh = data_mesh.drop_vars('gradient_sca_y')
-data_mesh = data_mesh.drop_vars('nlevels')
-data_mesh = data_mesh.drop_vars('nlevels_nod2D')
-data_mesh = data_mesh.drop_vars('nod_area')
-data_mesh = data_mesh.drop_vars('nod_in_elem2D')
-data_mesh = data_mesh.drop_vars('nod_in_elem2D_num')
-data_mesh = data_mesh.drop_vars('nod_part')
-data_mesh = data_mesh.drop_vars('nodes')
-data_mesh = data_mesh.drop_vars('zbar_e_bottom')
-data_mesh = data_mesh.drop_vars('zbar_n_bottom')
+mesh_data = mesh_data.drop_vars('edge_cross_dxdy')
+mesh_data = mesh_data.drop_vars('edge_tri')
+mesh_data = mesh_data.drop_vars('edges')
+mesh_data = mesh_data.drop_vars('elem_area')
+mesh_data = mesh_data.drop_vars('elem_part')
+mesh_data = mesh_data.drop_vars('gradient_sca_x')
+mesh_data = mesh_data.drop_vars('gradient_sca_y')
+mesh_data = mesh_data.drop_vars('nlevels')
+mesh_data = mesh_data.drop_vars('nlevels_nod2D')
+mesh_data = mesh_data.drop_vars('nod_area')
+mesh_data = mesh_data.drop_vars('nod_in_elem2D')
+mesh_data = mesh_data.drop_vars('nod_in_elem2D_num')
+mesh_data = mesh_data.drop_vars('nod_part')
+mesh_data = mesh_data.drop_vars('nodes')
+mesh_data = mesh_data.drop_vars('zbar_e_bottom')
+mesh_data = mesh_data.drop_vars('zbar_n_bottom')
 
 # Each row in the following variable is a triangle, and each value in the row is the index of a vertex
-elements = (data_mesh.elements.data.astype('int32') - 1).T
+elements = (mesh_data.elements.data.astype('int32') - 1).T
 elem_mask = []
 
 # Removing the triangles that have at least a vertex missing from the spatial subset
@@ -54,7 +54,9 @@ for v1, v2, v3 in elements:
         elem_mask.append(False)
     # TODO: filter also the edge data if the edge data ends up being useful
 
-data_mesh = data_mesh.drop_vars('elements')
-data_mesh['elements'] = (('elem_subset', 'nz3'), elements[elem_mask])
 
-#data_mesh.to_netcdf('./pilot_data.nc', engine='netcdf4') # The orignal mesh is 8.5GB, this is 337M
+
+mesh_data = mesh_data.drop_vars('elements')
+mesh_data['elements'] = (('elem_subset', 'nz3'), elements[elem_mask])
+
+#mesh_data.to_netcdf('./pilot_data.nc', engine='netcdf4') # The orignal mesh is 8.5GB, this is 3M
