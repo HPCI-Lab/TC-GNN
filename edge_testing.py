a4dc7b522ab1c00bedc859d6f7148f58fab6726a
massimilianofronza <maxfronza97@gmail.com>
2022-10-02 15:26:17 +0200

    Plot of links crossing others

--- edge_testing.py
+++ edge_testing.py
@@ -8,24 +8,35 @@ mesh_data = xr.open_dataset('./data/fesom.mesh.diag.nc')
 model_lon = mesh_data.lon.values
 model_lat = mesh_data.lat.values
 
-step = 2
+links = 13
 
-edge_0 = mesh_data.edges[0].values[:step]
-edge_1 = mesh_data.edges[1].values[:step]
+# Taking the first "links" connections
+edge_0 = mesh_data.edges[0].values[:links]
+edge_1 = mesh_data.edges[1].values[:links]
 #edge_tri = mesh_data.edge_tri
 
+# First the connection starting points
 x_0 = mesh_data.lon[edge_0].values
 y_0 = mesh_data.lat[edge_0].values
+
+# Then the connection endpoints
 x_1 = mesh_data.lon[edge_1].values
 y_1 = mesh_data.lat[edge_1].values
 
-# Putting the lines in the correct format for visualization
+# Organizing the start and ending connection points for visualization
 lines = []
-for i in range(step):
+for i in range(links):
     lines.append([(x_0[i], y_0[i]), (x_1[i], y_1[i])])
 
-lc = mc.LineCollection(lines, linewidths=2)
+# Color mask to highlight the last line
+c = [(0, 1, 0, 1)]*(links-1)
+c.append((1, 0, 0, 1))
+
+lc = mc.LineCollection(lines, colors=c, linewidths=1)
 fig, ax = pl.subplots()
 ax.add_collection(lc)
-ax.autoscale()
+#for i in range(links):
+#    plt.annotate(f"start_{i}", (x_0[i], y_0[i]))
+#    plt.annotate(f"end_{i}", (x_1[i], y_1[i]))
+ax.margins(0.1)
 plt.show()
