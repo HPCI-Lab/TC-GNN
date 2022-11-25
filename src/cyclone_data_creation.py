5c5f08a0f962e4af477bf0b24ba305a327b5372a
massimilianofronza <maxfronza97@gmail.com>
2022-11-25 19:23:01 +0100

    Add nodes processing and consistency check in the new pilot generator

--- src/cyclone_data_creation.py
+++ src/cyclone_data_creation.py
@@ -35,6 +35,7 @@ mesh_data = mesh_data.drop_vars('nodes')
 mesh_data = mesh_data.drop_vars('zbar_e_bottom')
 mesh_data = mesh_data.drop_vars('zbar_n_bottom')
 
+
 ### EDGES PROCESSING ###
 
 # Decrease by 1 because for compatibility with Fortran the indexes start from 1, not from 0
@@ -51,7 +52,42 @@ for i in range(len(edge_0)):
 
 edges_subset = np.array(edges_subset, dtype="int32")
 mesh_data = mesh_data.drop_vars('edges')
-mesh_data['edges'] = (('edge_subset', 'n2'), edges_subset)
+mesh_data['edges'] = (('edges_subset', 'n2'), edges_subset)
+
+
+### NODES PROCESSING ### - just adds 3MB to the pivot file. TODO: you could also just derive the nodes from the set of edges(see check_nodes_edges() for details)
+
+nodes_subset = []
+for i in range(len(region_mask)):
+    if region_mask[i]:
+        nodes_subset.append(i)
+
+nodes_subset = np.array(nodes_subset, dtype="int32")
+mesh_data['nodes'] =(('nodes_subset'), nodes_subset)
+
+
+### DEBUG FUNCTION ###
+
+# Check if the connection nodes and the nodes subset contain exactly the same set of nodes:
+def check_nodes_edges():
+    edge_0 = edges_subset[:, 0]                     # Take first column
+    edge_1 = edges_subset[:, 1]                     # Take second column
+    join_sides = np.concatenate((edge_0, edge_1))   # Merge them
+    join_sides = np.unique(join_sides)              # Pick unique elements
+    join_sides.sort()                               # Sort the lists
+    nodes_subset.sort()
+    res = True
+    counter = 0
+    for i in range(len(join_sides)):                # Assuming join_sides and nodes_subset have the same length
+        if join_sides[i] != nodes_subset[i]:
+            res = False
+        else:
+            counter += 1                            # This is a double-check to ensure that the verification process was effective
+    if res & (counter == len(join_sides)):
+        print(f"Edges and nodes contain the same {len(join_sides)} and {len(nodes_subset)} nodes")
+    else:
+        print("Something went really bad, need to debug")
+
 
 # Write the pilot mesh to the filesystem
-#mesh_data.to_netcdf('./pilot_mesh_cyclones.nc', engine='netcdf4') # The orignal mesh is 8.5GB, this is 153MB
+#mesh_data.to_netcdf('./pilot_mesh_cyclones.nc', engine='netcdf4') # The orignal mesh is 8.5GB, this is 156MB
