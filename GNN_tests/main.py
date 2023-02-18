1c96a527f387f329e46abf4dab431338d5faf154
massimilianofronza <maxfronza97@gmail.com>
2023-02-18 10:14:08 +0100

    Loss function test, visual() routine, major changes in train and test

--- GNN_tests/main.py
+++ GNN_tests/main.py
@@ -1,4 +1,6 @@
 # %%
+import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.metrics import f1_score
 import torch
 import torch_geometric
@@ -6,22 +8,25 @@ from torch_geometric.loader import DataLoader
 
 import Dataset
 import Models
-import summary  # should be torch_geometric.nn.summary, but it doesn't work
+from utils import time_func, summary  # should be torch_geometric.nn.summary, but it doesn't work
 
 print(f"Torch version: {torch.__version__}")
 print(f"Cuda available: {torch.cuda.is_available()}")
 print(f"Torch geometric version: {torch_geometric.__version__}")
 
-# Global variables
+### GLOBAL SETTINGS ###
+_model_name = "GUNet"
 _num_features = None
-_num_classes = Dataset.num_classes
-_model_name = "GCNet"
-_train_batch_size = 1
+_hidden_channels = 32   # 16 for GCNet, 32 for GUNet(from the examples)
+_num_classes = 1
+_train_batch_size = 5   # TODO see how the results change if you change this
 _test_batch_size = 5
 _valid_batch_size = 5
 
 # %% Creating the dataset
+timestamp = time_func.start_time()
 dataset = Dataset.PilotDataset(root='./data/bsc')#'./data/cmcc_structured')
+time_func.stop_time(timestamp, "Dataset creation")
 
 # Note that cyclone files start from 1, not form 0
 dataset.get(year=1983, cyclone=1).is_directed()
@@ -76,7 +81,7 @@ elif _model_name == "GUNet":
 
 model = Model(
     in_channels = _num_features,
-    hidden_channels = 32,   # 16 for GCNet, 32 for GUNet
+    hidden_channels = _hidden_channels,
     out_channels = _num_classes,
     data = train_loader.dataset[0]
 ).to(device)
@@ -86,51 +91,153 @@ print(f"\tdummy input: {train_loader.dataset[0]}")
 print(summary.summary(model, train_loader.dataset[0]))
 
 # %%
-loss_op = torch.nn.functional.nll_loss#torch.nn.BCEWithLogitsLoss()
+loss_op = torch.nn.CrossEntropyLoss()#torch.nn.functional.nll_loss#torch.nn.BCEWithLogitsLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 
 def train():
     model.train()
-
     total_loss = 0
-    for data in train_loader:
-        data = data.to(device)
+
+    for batch in train_loader:
+        batch = batch.to(device)
+
+        # zero the parameter gradients
         optimizer.zero_grad()
-        loss = loss_op(model(data), data.y)
-        total_loss += loss.item() * data.num_graphs
+
+        # forward + loss
+        pred = model(batch)
+        pred = pred.squeeze()
+        #print(np.shape(pred))
+        #print(np.shape(batch.y))
+        loss = loss_op(pred, batch.y)
+
+        # backward + optimize
+        # loss * _train_batch_size(5)
+        total_loss += loss.item() * batch.num_graphs
         loss.backward()
         optimizer.step()
-    return total_loss / len(train_loader.dataset)
+
+    # average loss = total_loss / training graps(20)
+    total_loss = total_loss / len(train_loader.dataset)
+    return total_loss
 
 loss = train()
 print(loss)
-# During the creation of train_loader:
-#   batch_size=1 leads to a loss of 64.45. Then 20626.77. Then 1203.
-#   batch_size=10 leads to a loss of 31.14. Then 228.81. It doesn't seem to depend by the shuffle option
 
-# %% Let's define the testing
+# %% 
+# torch.no_grad() as decorator is useful because I'm testing, so not calling 
+# the backward(). Mouse on it and read the doc
 @torch.no_grad()
 def test(loader):
     model.eval()
 
     ys, preds = [], []
-    for data in loader:
-        ys.append(data.y)
-        out = model(data.to(device))
-        preds.append((out > 0).float().cpu())
+    for batch in loader:
+        ys.append(batch.y)
+        batch = batch.to(device)
+
+        outputs = model(batch)  # shape: [8000, 1]
+        outputs = outputs.squeeze()
+        #print(np.shape(outputs))
+        preds.append((outputs > 0).float().cpu())   # "> 0" mette a bool
 
     y, pred = torch.cat(ys, dim=0).numpy(), torch.cat(preds, dim=0).numpy()
+    #print(np.shape(ys[1]))
+    #print(pred)
+    #print(outputs.sum())
+    #print(pred.sum())
     return f1_score(y, pred, average='micro') if pred.sum() > 0 else 0
 
 f1_res = test(test_loader)
 print(f1_res)
 
 # %% Everything together
-for epoch in range(100):
+timestamp = time_func.start_time()
+
+for epoch in range(50):
     loss = train()
     valid_f1 = test(valid_loader)
-    test_f1 = test(test_loader)
-    print(f'Epoch: {epoch+1:03d}, Loss: {loss:.4f}, Val: {valid_f1:.4f}, '
-          f'Test: {test_f1:.4f}')
+    print(f'Epoch: {epoch+1:03d}, Loss: {loss:.4f}, Val: {valid_f1:.4f}')
+
+time_func.stop_time(timestamp, "Training Complete!")
+
+test_f1 = test(test_loader)
+print(f'Test F1: {test_f1:.4f}')
+
+
+
+
+
+
+
+
+
+# %% 
+@torch.no_grad()
+def visual():
+    model.eval()
+
+    ys, preds = [], []
+    for batch in test_loader:
+        ys.append(batch.y)
+        batch = batch.to(device)
+
+        outputs = model(batch)  # shape: [8000, 1]
+        outputs = outputs.squeeze()
+        preds.append((outputs > 0).float().cpu())   # "> 0" mette a bool
+        
+        mat_pred = np.zeros(shape=(40, 40))
+        mat_target = np.zeros(shape=(40, 40))
+
+        patch_id = 0
+        index = 0+1600*patch_id
+        for lon in range(40):
+            for lat in range(40):
+                mat_pred[lat, lon] = outputs[index].item()
+                mat_target[lat, lon] = batch.y[index].item()
+                index += 1
+
+        fig, axs = plt.subplots(2, 1, figsize=(6, 6))
+        ax0 = axs[0].matshow(mat_pred)
+        ax1 = axs[1].matshow(mat_target)
+        fig.colorbar(ax0)
+        fig.colorbar(ax1)
+        plt.show()
+        #print(test_set[0].y, '\t', np.shape(test_set[0].y))
+        print("Pred:\t", outputs[:], '\n\t', np.shape(outputs[:]))
+        print("Target:\t", batch.y[:], '\n\t', np.shape(batch.y[:]))
+
+        #y, pred = torch.cat(ys, dim=0).numpy(), torch.cat(preds, dim=0).numpy()
+
+visual()
+
+
+
+
+
+
+
+
+
 
 # %%
+# Test from here: https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html#full-implementation
+@torch.no_grad()
+def my_test(loader):
+    num_samples = len(loader.dataset)
+    num_batches = len(loader)
+    test_loss, correct = 0, 0
+
+    for batch in loader:
+        batch.to(device)
+
+        pred = model(batch)
+        test_loss += loss_op(pred, batch.y).item()
+        correct += (pred.argmax(1) == batch.y).type(torch.float).sum().item()
+    
+    test_loss /= num_batches
+    correct /= num_samples
+    print(correct)
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+my_test(test_loader)        
