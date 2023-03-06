6c48983530964b8cea11ca7c070cef6d69d8ecf0
massimilianofronza <maxfronza97@gmail.com>
2023-03-06 15:56:27 +0100

    monitor.sh

--- GNN_tests/cluster/monitor.sh
+++ GNN_tests/cluster/monitor.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-watch -n 2 'squeue && echo '' && ls ../data/gnn_records/raw/ | wc -l'
-
+watch -n 2 'squeue && echo '/raw files:' && ls ../data/gnn_records/raw/ | wc -l && echo '/processed files:' && ls ../data/gnn_records/processed/ | wc -l'
