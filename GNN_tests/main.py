e852ce11f3be9e9f11acc2aff265a74aca166a58
massimilianofronza <maxfronza97@gmail.com>
2023-03-17 18:46:34 +0100

    Minor improvements

--- GNN_tests/main.py
+++ GNN_tests/main.py
@@ -15,6 +15,8 @@ from utils import time_func, summary  # should be torch_geometric.nn.summary, bu
 
 print(f"Torch version: {torch.__version__}")
 print(f"Cuda available: {torch.cuda.is_available()}")
+if torch.cuda.is_available():
+    print(f"Cuda device: {torch.cuda.get_device_name()}")
 print(f"Torch geometric version: {torch_geometric.__version__}")
 
 ### GLOBAL SETTINGS ###
@@ -36,6 +38,7 @@ _num_classes = 1
 _train_batch_size = 512   # TODO see how the results change if you change this
 _test_batch_size = 512
 _valid_batch_size = 512
+FINAL_ACTIVATION = torch.sigmoid#torch.nn.Linear(1, 1)#
 GRAPH_DICT = {}
 
 TIMESTAMP = time_func.start_time()
@@ -81,6 +84,7 @@ print("Dictionary of graph years and number of cyclones:\n", GRAPH_DICT)
 train_average, valid_total, test_total = 0, 0, 0
 for year in TRAIN_SET:
     train_average += GRAPH_DICT[str(year)]
+
 train_average /= len(TRAIN_SET)
 
 valid_total = int(train_average*20/80)
@@ -165,7 +169,8 @@ model = Model(
     in_channels = _num_features,
     hidden_channels = _hidden_channels,
     out_channels = _num_classes,
-    data = train_loader.dataset[0]      # Just to initialize the model
+    num_nodes = train_loader.dataset[0].num_nodes,  # just initialization
+    final_act = FINAL_ACTIVATION
 ).to(DEVICE)
 
 #print("\nSummary of NN:")
@@ -187,7 +192,6 @@ loss_op = torch.nn.BCELoss()            # works best with _train_size=100, highe
 #loss_op = torch.nn.L1Loss()
 #loss_op = torch.nn.MSELoss()
 
-print(f"Loss created!\n\tIn use: {loss_op}")
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 
 def train():
@@ -199,13 +203,13 @@ def train():
 
         # zero the parameter gradients
         optimizer.zero_grad()
-        
+
         # forward + loss
         pred = model(batch)
         pred = pred.squeeze()
 
         loss = loss_op(pred, batch.y)
-        
+
         # If you try the Soft Dice Score, use this(even if the loss stays constant)
         #loss.requires_grad = True
         #loss = torch.tensor(loss.item(), requires_grad=True)
@@ -257,6 +261,12 @@ print("Test loss, debug: ", loss)
 time_func.stop_time(TIMESTAMP, "Computation before training finished!")
 
 # %%
+# Settings recap
+print(f"Type of target: {LABEL_TYPE}")
+print(f"Final act: {FINAL_ACTIVATION}")
+print(f"Loss function: {loss_op}")
+
+# %%
 # Epoch training, validation and testing
 timestamp = time_func.start_time()
 
@@ -281,6 +291,8 @@ plt.figure(figsize=(8, 4))
 plt.plot(train_loss, label='Train loss')
 plt.plot(valid_loss, label='Validation loss')
 plt.legend(title="Loss type: " + str(loss_op))
+#ax = plt.gca()
+#ax.yaxis.set_major_formatter(lambda x, pos: f'{abs(x):g}')
 if ON_CLUSTER:
     plt.savefig('./images/train_valid_losses.png')
     plt.close()
@@ -300,13 +312,12 @@ def visual():
     pred = pred.squeeze()
     
     # Preparing the plot
-    fig, axs = plt.subplots(ncols=4, nrows=2, figsize=(16, 6))
-    #fig.suptitle('Predictions / Ground Truths', fontsize=14, y=0.95)
-    col = 0
+    fig, axs = plt.subplots(ncols=2, nrows=5, figsize=(6, 14))
+    row = 0
 
     # Take 4 patches from the batch
-    shift = 19
-    for patch_id in range(0+shift, 4+shift):#len(test_loader.dataset)):
+    shift = 20
+    for patch_id in range(0+shift, 5+shift):#len(test_loader.dataset)):
 
         # Allocate the empty prediction and target matrices
         mat_pred = np.zeros(shape=(40, 40))
@@ -320,16 +331,16 @@ def visual():
                 mat_target[lat, lon] = batch.y[index].item()
                 index += 1
 
-        ax_pred = axs[0, col].matshow(mat_pred)
-        ax_target = axs[1, col].matshow(mat_target)
-        col += 1
-        fig.colorbar(ax_pred, orientation='vertical')#, format='%.0e')#location='top'
-        fig.colorbar(ax_target, orientation='vertical')#location='top'
-
-    #plt.subplots_adjust(hspace=0.3, wspace=0.3)    # 5x2 setup
-    plt.subplots_adjust(hspace=0.1, wspace=0.3)    # 2x5 setup
+        ax_target = axs[row, 0].matshow(mat_target)
+        ax_pred = axs[row, 1].matshow(mat_pred)
+        row += 1
+        fig.colorbar(ax_pred, fraction=0.046, pad=0.04)#, format='%.0e')
+        fig.colorbar(ax_target, fraction=0.046, pad=0.04)
+    
+    plt.subplots_adjust(hspace=0.05, wspace=0.4)    # 5x2 setup
+    #plt.subplots_adjust(hspace=0.1, wspace=0.3)    # 2x5 setup
     if ON_CLUSTER:
-        plt.savefig('./images/testbatch4.png')
+        plt.savefig('./images/testbatch.png')
         plt.close(fig)
     else:
         plt.show()
